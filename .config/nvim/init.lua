8a3120daf8e7da8c86c1704806990254220c6bf6
huytang05 <tq.quanghuy@gmail.com>
2024-05-18 11:15:54 +0700

    save home

--- .config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("plugins.nice_to_have")
-require("plugins.ui")
-require("plugins.quality_of_life")
--- require("largefile")
